69850cdfe974a11e330ff9b004b211cac886042f
ratiry <ratiry@yandex.ru>
2022-10-20 14:24:43 +0300

    working version

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-let number = 14;
-let even_row = 3;
+let number = 17;
+let even_row = 7;
 let rating_where_to_start = (number,even_row)=>{
   let pairs = Math.floor(number/(even_row+even_row+1));
   let rows_sum =pairs*(even_row+even_row+1)+1;
@@ -13,7 +13,8 @@ let rating_where_to_start = (number,even_row)=>{
   return_array.push(row);
   return return_array; 
 }
-console.log( rating_where_to_start(number,even_row));
+let rating_array= rating_where_to_start(number,even_row);
+console.log(rating_array)
 let define_rows = (number,even_row)=>{
   let rows_array = [];
   let rows_sum = 0;
@@ -45,13 +46,47 @@ let define_rows = (number,even_row)=>{
   }
   return rows_array;
 }
-let seeking_for_element=(rows_array,number)=>{
+let define_rows_2 = (number,even_row,start_row,start_number)=>{
+  let rows_array = [];
+  let rows_sum = start_number;
+  let i=start_row;
+  let fork = true;
+  while(fork){
+    if(i%2==0){
+      let array=[];
+      let prevoius_rows_sum = rows_sum;
+      rows_sum=rows_sum+even_row;
+      for(let ii=prevoius_rows_sum;ii<rows_sum;ii++){
+        array.push(ii);
+      }
+      rows_array.push(array);
+      debugger;
+    }else{
+      let array=[];
+      let prevoius_rows_sum = rows_sum;
+      rows_sum=rows_sum+even_row+1;
+      for(let ii=prevoius_rows_sum;ii<rows_sum;ii++){
+        array.push(ii);
+
+      }
+      rows_array.push(array);
+      debugger;
+    }
+    if(rows_sum>=number){
+      fork=false;
+    }
+    i++;
+  }
+  return rows_array;
+}
+let seeking_for_element=(rows_array,number,row)=>{
   let permission_to_break_1 = false;
   let return_array = [];
   for(let i=0;i<rows_array.length;i++){
     for(let ii=0;ii<rows_array[i].length;ii++){
       if(rows_array[i][ii] == number){
-        return_array.push(i+1);
+        return_array.push(i+1+row);
+        debugger;
         return_array.push(ii+1);
         permission_to_break_1=true;
         break;
@@ -63,7 +98,8 @@ let seeking_for_element=(rows_array,number)=>{
   }
   return return_array;
 }
+let rows_array_2= define_rows_2(number,even_row,rating_array[1],rating_array[0]);
+let result = seeking_for_element(rows_array_2,number,rating_array[1]);
+console.log(result);
 let rows_array_= define_rows(number,even_row);
 console.log(rows_array_);
-let place = seeking_for_element(rows_array_,number);
-console.log(place)
